0ce9c682224cf6c35ae4416b87544d9a1d6d8ae0
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-03-13 14:58:27 -0600

    chore: update constants by using data from dotenv

--- page_model/data/Constants.js
+++ page_model/data/Constants.js
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const CREDENTIALS = {
     VALID_USER: {
-        USERNAME: 'testuser@example.com',
-        PASSWORD: 'test123',
+        USERNAME: process.env.VALID_USERNAME,
+        PASSWORD: process.env.VALID_PASSWORD,
     },
     INVALID_USER: {
-        USERNAME: 'testuser@example.com',
-        PASSWORD: 'test1234'
+        USERNAME: 'invalid_username',
+        PASSWORD: 'invalid_password'
     }
 }
