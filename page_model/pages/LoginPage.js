3ceedda959a4f1699d9d00325cc233739320bf32
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-03-13 13:56:02 -0600

    chore: add basic login test

--- page_model/pages/LoginPage.js
+++ page_model/pages/LoginPage.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe'
 
 class LoginPage {
     constructor(){
-        this.userNamefield = Selector('input[name="login.username"]')
+        this.userNameField = Selector('input[name="login.username"]')
         this.passwordField = Selector('input[name="login.password"]')
         this.loginButton = Selector('.btn.btn-primary')
         this.errorMessage = Selector('#login-error-message')
